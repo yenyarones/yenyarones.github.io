525dee6080a7ed69ce703cc809d7cfeac2c8af12
yenyarones <yeny.be@gmail.com>
2024-10-26 10:40:59 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const greeting = document.getElementById("greeting");
+
+greeting.style.textAlign = "center";
+greeting.style.marginTop = "50px";
+greeting.style.fontSize = "50px";
+greeting.style.color = "3a3139";
+greeting.style
+const username = window.prompt("what is your name?","or nickname");
+
+greeting.innerHTML ="welcome to my website, " +username+"!" ;
